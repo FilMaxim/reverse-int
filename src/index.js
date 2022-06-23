1b0cf8b98a33d05ec20bec74ee3dd2c964c67985
FilMaxim <fil.maxim13@gmail.com>
2022-06-23 15:51:05 +0300

    init: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = String(Math.abs(n));
+    let long = str.length;
+    let result = "";
+    for (let i = 0; i < long; i++) {
+        result = `${str[i]}${result}`;
+    }
+    return result;
 }
